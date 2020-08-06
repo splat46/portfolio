e770b860837ff448ee33aa66a5d62646572ea2e7
splat46 <stijnhuiskes@gmail.com>
2020-08-06 23:20:47 +0200

    Installed react router dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <Switch>
+        <Route path="/" />
+      </Switch>
     </div>
   );
 }
