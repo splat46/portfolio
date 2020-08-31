84e95346b22d286da1292310065d6d90bbb25c13
splat46 <stijnhuiskes@gmail.com>
2020-08-31 09:38:34 +0200

    Some styling and setup signup page

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -13,33 +13,33 @@ export default function LoginPage() {
   }
 
   return (
-    <div className="text-center">
-      <h1>Login Bitch</h1>
-      <form onSubmit={handleSubmit}>
-        <p>
-          <label>
-            Email:{" "}
-            <input
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </label>
-        </p>
-        <p>
-          <label>
-            Password:{" "}
-            <input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </label>
-        </p>
-        <p>
-          <button type="submit">Login</button>
-        </p>
-      </form>
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="col-md-5">
+          <h1>Login Bitch</h1>
+          <form className="box box__padding box--grey" onSubmit={handleSubmit}>
+            <p>
+              <label>Email: </label>
+              <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </p>
+            <p>
+              <label>Password: </label>
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </p>
+            <p>
+              <button type="submit">Login</button>
+            </p>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
--- src/pages/SignupPage.js
+++ src/pages/SignupPage.js
@@ -3,8 +3,57 @@ import React, { useState } from "react";
 
 export default function SignupPage() {
   return (
-    <div className="text-center">
-      <h1>Signup Bitch</h1>
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="col-md-5">
+          <h1>Signup Bitch</h1>
+
+          <div className="box box__padding box--grey">
+            <form>
+              <div className="form-group row">
+                <label className="col-sm-4 col-form-label">Firstname</label>
+                <div className="col-sm-8">
+                  <input type="text" name="firstname"></input>
+                </div>
+              </div>
+              <div className="form-group row">
+                <label className="col-sm-4 col-form-label">Lastname</label>
+                <div className="col-sm-8">
+                  <input type="text" name="lastname"></input>
+                </div>
+              </div>
+              <div className="form-group row">
+                <label className="col-sm-4 col-form-label">Phone</label>
+                <div className="col-sm-8">
+                  <input type="text" name="phone"></input>
+                </div>
+              </div>
+              <div className="form-group row">
+                <label className="col-sm-4 col-form-label">Email</label>
+                <div className="col-sm-8">
+                  <input
+                    type="text"
+                    name="email"
+                    placeholder="example@email.com"
+                  ></input>
+                </div>
+              </div>
+              <div className="form-group row">
+                <label className="col-sm-4 col-form-label">Password</label>
+                <div className="col-sm-8">
+                  <input type="password" name="password"></input>
+                </div>
+              </div>
+              <button type="submit" className="btn btn-primary mr-4">
+                Signup
+              </button>
+              <a href="/#" alt="Already a account? Sign in">
+                I already have a account
+              </a>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
